979f64a02e70cb8f831fa7eae815f2f333454347
elentkachenko <77787424+Elena23Tkachenko@users.noreply.github.com>
2021-09-30 15:21:14 +0300

    hw5

--- src/com/itAcademy/Main.java
+++ src/com/itAcademy/Main.java
@@ -1,57 +1,23 @@
 package com.itAcademy;
 
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) {
-        String s = "\"configGlossary:installationAt\": \"Philadelphia, PA\",\n" +
-                "\"configGlossary:adminEmail\": \"ksm@pobox.com\",\n" +
-                "\"configGlossary:poweredBy\": \"Cofax\",\n" +
-                "\"configGlossary:poweredByIcon\": \"/images/cofax.gif\",\n" +
-                "\"configGlossary:staticPath\": \"/content/static\",\n" +
-                "\"templateProcessorClass\": \"org.cofax.WysiwygTemplate\",\n" +
-                "\"templateLoaderClass\": \"org.cofax.FilesTemplateLoader\",\n" +
-                "\"templatePath\": \"templates\",\n" +
-                "\"templateOverridePath\": \"\",\n" +
-                "\"defaultListTemplate\": \"listTemplate.htm\",\n" +
-                "\"defaultFileTemplate\": \"articleTemplate.htm\",\n" +
-                "\"useJSP\": false,\n" +
-                "\"jspListTemplate\": \"listTemplate.jsp\",\n" +
-                "\"jspFileTemplate\": \"articleTemplate.jsp\",\n" +
-                "\"cachePackageTagsTrack\": 200,\n" +
-                "\"cachePackageTagsStore\": 200,\n" +
-                "\"cachePackageTagsRefresh\": 60,\n" +
-                "\"cacheTemplatesTrack\": 100,\n" +
-                "\"cacheTemplatesStore\": 50,\n" +
-                "\"cacheTemplatesRefresh\": 15,\n" +
-                "\"cachePagesTrack\": 200,\n" +
-                "\"cachePagesStore\": 100,\n" +
-                "\"cachePagesRefresh\": 10,\n" +
-                "\"cachePagesDirtyRead\": 10,\n" +
-                "\"searchEngineListTemplate\": \"forSearchEnginesList.htm\",\n" +
-                "\"searchEngineFileTemplate\": \"forSearchEngines.htm\",\n" +
-                "\"searchEngineRobotsDb\": \"WEB-INF/robots.db\",\n" +
-                "\"useDataStore\": true,\n" +
-                "\"dataStoreClass\": \"org.cofax.SqlDataStore\",\n" +
-                "\"redirectionClass\": \"org.cofax.SqlRedirection\",\n" +
-                "\"dataStoreName\": \"cofax\",\n" +
-                "\"dataStoreDriver\": \"com.microsoft.jdbc.sqlserver.SQLServerDriver\",\n" +
-                "\"dataStoreUrl\": \"jdbc:microsoft:sqlserver://LOCALHOST:1433;DatabaseName=goon\",\n" +
-                "\"dataStoreUser\": \"sa\",\n" +
-                "\"dataStorePassword\": \"dataStoreTestQuery\",\n" +
-                "\"dataStoreTestQuery\": \"SET NOCOUNT ON;select test='test';\",\n" +
-                "\"dataStoreLogFile\": \"/usr/local/tomcat/logs/datastore.log\",\n" +
-                "\"dataStoreInitConns\": 10,";
 
-        Pattern pattern = Pattern.compile("\\w+@\\w+\\.\\w+");
-        Matcher matcher = pattern.matcher(s);
-        String result;
-        while (matcher.find()) {
-            result = matcher.group();
-            System.out.println(result);
-
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите данные: ");
+        String data = in.nextLine();
+        Pattern pattern = Pattern.compile("[-.?\\w]+@[-\\w]+\\.[-\\w]+");
+        Matcher matcher = pattern.matcher(data);
+        if (matcher.find()) {
+            System.out.println("Электронная почта: " + matcher.group());
+        } else {
+            System.out.println("Электронная почта не найдена.");
         }
     }
 }
+
